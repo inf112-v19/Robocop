ba3703797830e26ff6395d8095085c6dae65a8cf
Steffen Grønmo <sgr061@post.uit.no>
2019-03-01 19:51:18 +0100

    Chatbox should now work + added input field with skin from libgdx/tests

--- src/main/java/inf112/skeleton/app/menu/ScrollableTextbox.java
+++ src/main/java/inf112/skeleton/app/menu/ScrollableTextbox.java
@@ -9,53 +9,82 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /*
  * Circular message-box.
  */
 public class ScrollableTextbox {
     TextField[] lines;
-    int         lineAmount,
-                lineLimit;
-
-    TextField.TextFieldStyle txtStyle;
-    TextField emptyField;
+    int         lineAmount = 0,
+                lineLimit,
+                displayFrom = 0,
+                numFields = 5;
 
     Stage stage;
     Table display;
 
-    int displayFrom;
-
     ImageButton button_up;
     ImageButton button_down;
 
+    TextField.TextFieldStyle txtStyle;
+    TextField inputField;
+    Actor emptyField;
 
     int tableWidth = 600,
         tableHeight = 140;
 
     public ScrollableTextbox(int limit, InputMultiplexer inputMultiplexer) {
-        lines = new TextField[limit];
-        lineAmount = displayFrom = 0;
         lineLimit = limit;
+
+        init_inputField();
+        init_scrollButtons();
+        emptyField = new Actor();
 
         txtStyle = new TextField.TextFieldStyle();
         txtStyle.font = new BitmapFont();
         txtStyle.fontColor = Color.YELLOW;
-        emptyField = new TextField("", txtStyle);
 
-        stage = new Stage();
-        inputMultiplexer.addProcessor(stage);
+        lines = new TextField[limit];
+        for (int i = 0 ; i < limit ; i++) {
+            lines[i] = new TextField("", txtStyle);
+            lines[i].setDisabled(true);
+        }
 
         display = new Table();
-        //display.setFillParent(true); // Automatic resize and placement on screen
-        display.setDebug(false);      // Display corners of table-cells/buttons
+        display.setDebug(false);
         display.setSize(tableWidth,tableHeight);
+        display.background(new TextureRegionDrawable(new TextureRegion(
+                new Texture(Gdx.files.internal("chatStyleOpac.png")))));
+        updateDisplay();
 
+        stage = new Stage(new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+        stage.addActor(display);
+        inputMultiplexer.addProcessor(stage);
+    }
+
+    private void init_inputField() {
+        inputField = new TextField("", new Skin(Gdx.files.internal("uiskin.json")));
+        inputField.setMessageText("Type a message...");
+        inputField.setTextFieldListener(new TextField.TextFieldListener() {
+            @Override
+            public void keyTyped(TextField textField, char key) {
+                if ((key == '\r' || key == '\n')) {
+                    String inputText = inputField.getText();
+                    if (!inputText.equals("")) {
+                        push(inputText);
+                    }
+                    inputField.setText("");
+                    stage.setKeyboardFocus(null);
+                }
+            }
+        });
+    }
+
+    private void init_scrollButtons() {
         button_up   = new ImageButton(new TextureRegionDrawable(new TextureRegion(
                 new Texture(Gdx.files.internal("triangleBlack.png")))));
         button_down = new ImageButton(new TextureRegionDrawable(new TextureRegion(
@@ -65,7 +94,7 @@ public class ScrollableTextbox {
                 new ChangeListener() {
                     @Override
                     public void changed(ChangeEvent changeEvent, Actor actor) {
-                        System.out.println("Hello dude");
+                        scrollDisplay(-1);
                     }
                 }
         );
@@ -73,43 +102,55 @@ public class ScrollableTextbox {
                 new ChangeListener() {
                     @Override
                     public void changed(ChangeEvent changeEvent, Actor actor) {
-                        System.out.println("Hello dude");
+                        scrollDisplay(1);
                     }
                 }
         );
-
-        int arrowButtonSize = (tableHeight - 10) / 5;
-
-        // Add empty cells to the table.
-        display.add(emptyField).width(tableWidth-arrowButtonSize - 10).padBottom(1).padTop(3).uniform();
-        display.add(button_up).width(arrowButtonSize).height(arrowButtonSize).padRight(4);
-        display.row();
-        for (int i = 0 ; i < 3 ; i++) {
-            display.add(emptyField).width(tableWidth-arrowButtonSize - 10).padBottom(1).uniform();
-            display.row();
-        }
-        display.add(emptyField).width(tableWidth-arrowButtonSize - 10).padBottom(4).uniform();
-        display.add(button_down).width(arrowButtonSize).height(arrowButtonSize).padRight(4);
-        display.row();
-        display.background(new TextureRegionDrawable(new TextureRegion(
-                new Texture(Gdx.files.internal("chatStyleOpac.png")))));
-
-
-        stage.addActor(button_up);
-        //inputContainer.addActor(button_down);
     }
 
+
+    public void scrollDisplay(int scrollAmount) {
+        if (scrollAmount < 0) {
+            scrollAmount = -((-scrollAmount) % lineLimit);
+        }
+        displayFrom = (displayFrom + scrollAmount + lineLimit) % lineLimit;
+
+        updateDisplay();
+    }
+
+
     public void push(String str) {
-        lines[lineAmount++ % lineLimit] = new TextField(str, txtStyle);
+        lines[++lineAmount % lineLimit].setText(str);
+        if (lineAmount % lineLimit == (displayFrom + 1) % lineLimit)
+            scrollDisplay(1);
+    }
+
+
+    public void updateDisplay() {
+        int arrowButtonSize = (tableHeight - 10) / numFields;
+
+        display.clearChildren();
+
+        // Add empty cells to the table.
+        display.add(lines[(displayFrom-numFields+2+lineLimit) % lineLimit]).width(tableWidth-arrowButtonSize - 20).padLeft(10).padBottom(1).padTop(3).uniform();
+        display.add(button_up).width(arrowButtonSize).height(arrowButtonSize).padRight(4);
+        display.row();
+        for (int i = numFields-3 ; i >= 0 ; i--) {
+            display.add(lines[(displayFrom-i+lineLimit) % lineLimit]).width(tableWidth-arrowButtonSize - 20).padLeft(10).padBottom(1).uniform();
+            display.add(emptyField);
+            display.row();
+        }
+
+        display.add(inputField).width(tableWidth-arrowButtonSize - 30).height(arrowButtonSize-4).padLeft(5).padBottom(4).uniform();
+        display.add(button_down).width(arrowButtonSize).height(arrowButtonSize).padRight(4);
+        display.row();
     }
 
     public void render(Batch sb) {
-        display.draw(sb,1);
-        button_up.draw(sb,1);
+        stage.draw();
     }
 
     public void setPosition(int x, int y) {
         display.setPosition(x,y);
     }
-
 }
