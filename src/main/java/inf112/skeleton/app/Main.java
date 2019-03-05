11b1242a3bd349e8d6a9ce72cffe52106ec1aea4
Promises <henning.ber@gmail.com>
2019-03-05 02:42:00 +0100

    main now runs the client

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -3,33 +3,12 @@ package inf112.skeleton.app;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3WindowConfiguration;
+import inf112.skeleton.app.Socket.Client;
 import inf112.skeleton.server.Server;
 
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        boolean server = false;
-        if (args.length > 0) {
-            if (args[0].equalsIgnoreCase("true")) {
-                server = true;
-            }
-        }
-
-
-        if (!server) {
-            // TODO: Ask Henning why Lwjgl3 is used
-
-            Lwjgl3ApplicationConfiguration application = new Lwjgl3ApplicationConfiguration();
-            application.setWindowedMode(RoboRally.WIDTH,RoboRally.HEIGHT);
-            application.setTitle(RoboRally.TITLE);
-            //cfg.title = RoboRally.TITLE;
-            //cfg.width = RoboRally.WIDTH;
-            //cfg.height = RoboRally.HEIGHT;
-
-            new Lwjgl3Application(new RoboRally(), application);
-        } else {
-            Server.run();
-        }
-
+        Client.main(args);
     }
 }
