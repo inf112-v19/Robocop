e0fb49f44a1617e2a95912ec67a4e6cc7afa3c4a
yap005 <yap005@student.uib.no>
2019-03-13 15:31:26 +0100

    A class to store sprites

---
+++ src/main/java/inf112/skeleton/app/board/entity/Sprites.java
@@ -0,0 +1,30 @@
+package inf112.skeleton.app.board.entity;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import inf112.skeleton.common.specs.Directions;
+
+public class Sprites {
+    public static TextureAtlas[] robotSprites;
+    public static Animation[][] animations;
+    public static void setup(){
+        robotSprites = new TextureAtlas[7];
+        animations = new Animation[7][4];
+
+        robotSprites[0] = new TextureAtlas(Gdx.files.internal("graphics/sprites/robots/robotAllSidesBlue2.atlas"));
+        robotSprites[1] = new TextureAtlas(Gdx.files.internal("graphics/sprites/robots/robotAllSidesDarkGreen.atlas"));
+        robotSprites[2] = new TextureAtlas(Gdx.files.internal("graphics/sprites/robots/robotAllSidesGreen.atlas"));
+        robotSprites[3] = new TextureAtlas(Gdx.files.internal("graphics/sprites/robots/robotAllSidesOrange.atlas"));
+        robotSprites[4] = new TextureAtlas(Gdx.files.internal("graphics/sprites/robots/robotAllSidesPink.atlas"));
+        robotSprites[5] = new TextureAtlas(Gdx.files.internal("graphics/sprites/robots/robotAllSidesRed.atlas"));
+        robotSprites[6] = new TextureAtlas(Gdx.files.internal("graphics/sprites/robots/robotAllSides.atlas"));
+        for (int i = 0; i < robotSprites.length; i++) {
+            animations[i][Directions.NORTH.ordinal()] = new Animation(0.1f, robotSprites[i].findRegions("robotAllSides_North"), Animation.PlayMode.LOOP);
+            animations[i][Directions.SOUTH.ordinal()] = new Animation(0.1f, robotSprites[i].findRegions("robotAllSides_South"), Animation.PlayMode.LOOP);
+            animations[i][Directions.EAST.ordinal()] = new Animation(0.1f, robotSprites[i].findRegions("robotAllSides_East"), Animation.PlayMode.LOOP);
+            animations[i][Directions.WEST.ordinal()] = new Animation(0.1f, robotSprites[i].findRegions("robotAllSides_West"), Animation.PlayMode.LOOP);
+        }
+    }
+
+}
