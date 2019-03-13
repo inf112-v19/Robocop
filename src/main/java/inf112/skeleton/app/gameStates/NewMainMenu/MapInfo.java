64f8bdf73d90e76e20c54d0c26600e5dcbb8b57b
Steffen Grønmo <sgr061@post.uit.no>
2019-03-13 20:31:33 +0100

    Added initializer to mapinfo-class

--- src/main/java/inf112/skeleton/app/gameStates/NewMainMenu/MapInfo.java
+++ src/main/java/inf112/skeleton/app/gameStates/NewMainMenu/MapInfo.java
@@ -14,8 +14,32 @@ public class MapInfo {
     String  mapName,
             hostName,
             description;
-    int     maxPlayers,
+    int     numPlayers,
+            maxPlayers,
             minRecommendedPlayers,
             maxRecommendedPlayers;
     Drawable minimap_preview;
+    MapDifficulty difficulty;
+
+    public MapInfo(
+            String mapName,
+            String hostName,
+            String description,
+            int numPlayers,
+            int maxPlayers,
+            int minRecommendedPlayers,
+            int maxRecommendedPlayers,
+            MapDifficulty difficulty,
+            Drawable minimap_preview
+    ){
+        this.mapName                = mapName;
+        this.hostName               = hostName;
+        this.description            = description;
+        this.numPlayers             = numPlayers;
+        this.maxPlayers             = maxPlayers;
+        this.minRecommendedPlayers  = minRecommendedPlayers;
+        this.maxRecommendedPlayers  = maxRecommendedPlayers;
+        this.difficulty             = difficulty;
+        this.minimap_preview        = minimap_preview;
+    }
 }
