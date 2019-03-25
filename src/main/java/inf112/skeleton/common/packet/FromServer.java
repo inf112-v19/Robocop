803f54c57bc60731a14bf1e62297893ce8e4ea21
Promises <henning.ber@gmail.com>
2019-03-25 23:48:23 +0100

    init client packet

--- src/main/java/inf112/skeleton/common/packet/FromServer.java
+++ src/main/java/inf112/skeleton/common/packet/FromServer.java
@@ -2,6 +2,7 @@ package inf112.skeleton.common.packet;
 
 public enum FromServer {
     LOGINRESPONSE,
+    INIT_CLIENT,
     INIT_PLAYER,
     INIT_ALLPLAYERS, //TODO: IMplement
     INIT_LOBBY, //TODO: IMplement
