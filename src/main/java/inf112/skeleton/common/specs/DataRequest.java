043fc5d90b0090ffd2ae5bf14bd9f814aab12d04
marenaleksandersen <zuff004@student.uib.no>
2019-03-27 15:12:31 +0100

    Added timing system for lobbies, startbbutton hadeled server side

--- src/main/java/inf112/skeleton/common/specs/DataRequest.java
+++ src/main/java/inf112/skeleton/common/specs/DataRequest.java
@@ -3,4 +3,5 @@ package inf112.skeleton.common.specs;
 public enum DataRequest {
     LOBBY_LIST,
     LOBBY_LEAVE,
+    LOBBY_START
 }
--- src/main/java/inf112/skeleton/common/specs/StateChange.java
+++ src/main/java/inf112/skeleton/common/specs/StateChange.java
@@ -2,6 +2,7 @@ package inf112.skeleton.common.specs;
 
 public enum StateChange {
     PLAYER_KICKED,
+    GAME_START
 
 
 }
