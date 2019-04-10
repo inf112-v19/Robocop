e84e70c5b038c19b527dd0dbd1e054f5eb89d762
Steffen Grønmo <sgr061@post.uit.no>
2019-04-10 23:34:31 +0200

    Updated laser directions...

--- src/main/java/inf112/skeleton/common/specs/Directions.java
+++ src/main/java/inf112/skeleton/common/specs/Directions.java
@@ -1,5 +1,7 @@
 package inf112.skeleton.common.specs;
 
+import com.badlogic.gdx.math.Vector2;
+
 public enum Directions {
     NORTH,
     EAST,
@@ -12,5 +14,24 @@ public enum Directions {
                 return val;
         }
         return null;
+    }
+
+    public static Vector2 vectorDirection(Directions direction) {
+        switch(direction) {
+            case NORTH:
+                return new Vector2(0,1);
+
+            case SOUTH:
+                return new Vector2(0,-1);
+
+            case EAST:
+                return new Vector2(1,0);
+
+            case WEST:
+                return new Vector2(-1,0);
+
+            default:
+                return null;
+        }
     }
 }
