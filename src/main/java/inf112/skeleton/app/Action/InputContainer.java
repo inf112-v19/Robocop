2f3098c62aaa27ce43e8978d966c4b0a603418be
marenaleksandersen <zuff004@student.uib.no>
2019-04-23 10:30:25 +0200

    change to fit java doc format

--- src/main/java/inf112/skeleton/app/Action/InputContainer.java
+++ src/main/java/inf112/skeleton/app/Action/InputContainer.java
@@ -2,12 +2,12 @@ package inf112.skeleton.app.Action;
 
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
-/*
-Each key on the keyboard (with some exceptions) are represented in Keys.class as an int between 0 and 255.
-To update key status, you change the specific index. To check if a key is pressed,
-you look up the boolean value at the key's index.
-This approach makes it much easier to implement additional keys,
-as you only need to add an additional line to the handleKeys method.
+/**
+ * Each key on the keyboard (with some exceptions) are represented in Keys.class as an int between 0 and 255.
+ * To update key status, you change the specific index. To check if a key is pressed,
+ * you look up the boolean value at the key's index.
+ * This approach makes it much easier to implement additional keys,
+ * as you only need to add an additional line to the handleKeys method.
 */
 public class InputContainer extends Stage {
     public boolean[] keys;
